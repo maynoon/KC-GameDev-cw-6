bfe087085a1c1a26c5e229ea5a2fa64babfb5200
bader 1043 <115097112+maynoon@users.noreply.github.com>
2022-10-26 18:15:59 +0300

    cw

---
+++ unity/Assets/script/player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    
+
+    // Update is called once per frame
+    void FixedUpdate()
+
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))  // If the player is pressing the "d" key
+        {
+            // Add a force to the right
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))  // If the player is pressing the "a" key
+        {
+            // Add a force to the left
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+}
---
+++ unity/Assets/script/playercolision.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class playercolision : MonoBehaviour
+{
+
+    public player movement;
+    
+    private void OnCollisionEnter (Collision collisioninfo)
+    {
+        
+
+        if(collisioninfo.collider.tag == "obstacle")
+        {
+
+
+            movement.enabled = false;   
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
